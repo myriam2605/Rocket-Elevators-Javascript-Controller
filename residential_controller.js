00695b0174ace4d3df184bf725feb68e180f2906
myriam2605 <98342743+myriam2605@users.noreply.github.com>
2022-02-21 15:01:33 -0500

    first one

--- residential_controller.js
+++ residential_controller.js
@@ -1,33 +1,23 @@
 class Column {
     constructor(_id, _amountOfFloors, _amountOfElevators) {
-
-    };
-
+        console.log(_id);
+    }
 }
 
 class Elevator {
-    constructor(_id, _amountOfFloors) {
-
-    }
-
+    constructor(_id, _amountOfFloors) {}
 }
 
 class CallButton {
-    constructor(_id, _floor, _direction) {
-
-    }
+    constructor(_id, _floor, _direction) {}
 }
 
 class FloorRequestButton {
-    constructor(_id, _floor) {
-
-    }
+    constructor(_id, _floor) {}
 }
 
 class Door {
-    constructor(_id) {
-
-    }
+    constructor(_id) {}
 }
 
-module.exports = { Column, Elevator, CallButton, FloorRequestButton, Door }
+module.exports = { Column, Elevator, CallButton, FloorRequestButton, Door };
