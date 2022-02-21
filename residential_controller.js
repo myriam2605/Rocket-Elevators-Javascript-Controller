e20a8e1748750e3afd5a6be899cc5903fe5f45f8
myriam2605 <98342743+myriam2605@users.noreply.github.com>
2022-02-21 15:48:33 -0500

    first try - arguments ok and all constructors are ok

--- residential_controller.js
+++ residential_controller.js
@@ -1,23 +1,40 @@
 class Column {
     constructor(_id, _amountOfFloors, _amountOfElevators) {
-        console.log(_id);
+        this.id = id;
+        this.amountOfFloors = _amountOfFloors;
+        this.amountOfElevators = _amountOfElevators;
     }
 }
 
 class Elevator {
-    constructor(_id, _amountOfFloors) {}
+    constructor(_id, _amountOfFloors) {
+        this.id = id;
+        this.amountOfFloors = amountOfFloors;
+    }
 }
 
 class CallButton {
-    constructor(_id, _floor, _direction) {}
+    constructor(_id, _floor, _direction) {
+        this.id = _id;
+        this.floor = _floor;
+        this.direction = _direction;
+    }
 }
 
 class FloorRequestButton {
-    constructor(_id, _floor) {}
+    constructor(_id, _floor) {
+        this.id = _id;
+        this.floor = _floor;
+    }
 }
 
 class Door {
-    constructor(_id) {}
+    constructor(_id) {
+        this.id = _id;
+    }
 }
+
+// var Column = new Column(1, 10, 2);
+// console.log(Column);
 
 module.exports = { Column, Elevator, CallButton, FloorRequestButton, Door };
